52e5b67a1eeb58d56b39df1c82a166c9af9f87d1
fmsarp <fmsarp@gmail.com>
2024-04-18 13:39:17 -0400

    clear and decimal button donfiguration

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ function sendNumberValue(number) {
     calculatorDisplay.textContent = displayValue ==='0' ? number : displayValue + number;
 }
 
+function addDeciaml() {
+    // if no decimal, add one
+    if (!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
+}
+
 // add event listners for numbers, operators, decimal buttons
 inputBtns.forEach((inputBtn) => {
     if (inputBtn.classList.length === 0) {
@@ -16,6 +23,14 @@ inputBtns.forEach((inputBtn) => {
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
     }
     else if (inputBtn.classList.contains('decimal')) {
-        inputBtn.addEventListener('click', () => sendNumberValue())
+        inputBtn.addEventListener('click', () => addDeciaml())
     }
 });
+
+// reset display
+function resetAll() {
+    calculatorDisplay.textContent = '0';
+};
+
+// event listener
+clearBtn.addEventListener('click', resetAll);
